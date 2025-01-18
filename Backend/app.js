1e5b8a7920ca4c66e485a9ca907bf6cd91eb2955
I=m-abhi <abhisasa2001@gmail.com>
2025-01-19 05:13:36 +0530

    User Profile Done

--- Backend/app.js
+++ Backend/app.js
@@ -9,11 +9,13 @@ const connectToDb = require("./db/db");
 connectToDb();
 
 const userRoutes = require("./routes/user.routes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/home", (req, res) => {
   res.send("hello world");
