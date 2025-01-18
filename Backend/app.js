ce609033fe75642cecd2c274d8ba9bd5bd294300
I=m-abhi <abhisasa2001@gmail.com>
2025-01-19 01:41:44 +0530

    User Registration done

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,24 @@
-// Setting dotenv variable
+// setting dotenv variable
 const dotenv = require("dotenv");
 dotenv.config();
 
 const express = require("express");
 const cors = require("cors");
+// connect to MongoDB Database
+const connectToDb = require("./db/db");
+connectToDb();
+
+const userRoutes = require("./routes/user.routes");
 
 const app = express();
-
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/home", (req, res)=> {
+app.get("/home", (req, res) => {
   res.send("hello world");
 });
+
+app.use("/users", userRoutes);
 
 module.exports = app;
